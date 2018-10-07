c63cd1f4871589d91a967110ca658ce2aa13fc9f
sandy epiard <sandy.epiard@ensiie.fr>
2018-10-07 16:00:29 +0200

    first commit (commit du tp kinect)

---
+++ Assets/Script/Kinect/faast/MoveFromKeyboard.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveFromKeyboard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton("Fire1"))
+            transform.position += new Vector3(0.1f, 0.0f);
+	}
+}
---
+++ Assets/Script/TraceDataRot.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class TraceDataRot : MonoBehaviour {
+    private string data;
+    private string folder;
+
+    //private KinectPointController k;
+
+    public GameObject right_hand;
+    public GameObject neck;
+
+    // Use this for initialization
+    void Start()
+    {
+        //folder = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
+        folder = "C:\\Users\\Takaruichi\\Documents\\Unity3D Projects\\tp_kinect\\traceRot.txt";
+        File.WriteAllText(folder, "");
+        //k = GetComponent<KinectPointController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (k.isTracked)
+        {*/
+            data = Time.time + " " + transform.rotation.x + " " + transform.rotation.y + " " + transform.rotation.z;
+            //File.WriteAllText(folder, data);
+            File.AppendAllText(folder, data + "\n");
+        //}
+    }
+}
