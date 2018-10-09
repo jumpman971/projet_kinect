c76ca7b21ce9278945a8f051ba19b19df8f40388
sandy epiard <sandy.epiard@ensiie.fr>
2018-10-09 17:26:40 +0200

    ajout du menu

---
+++ Assets/Script/SceneController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneController : MonoBehaviour {
+    public Button mainBtn;
+    public Button exitBtn;
+
+    // Use this for initialization
+    void Start () {
+        mainBtn.onClick.AddListener(loadMainScene);
+        exitBtn.onClick.AddListener(exitUnity);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void loadMainScene()
+    {
+        SceneManager.LoadScene("KinectSample");
+    }
+
+    void exitUnity()
+    {
+        Application.Quit();
+    }
+}
