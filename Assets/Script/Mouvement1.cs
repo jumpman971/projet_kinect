9bb4a3854917d0a683afa5bcef1df185811c8232
sandy epiard <sandy.epiard@ensiie.fr>
2018-10-12 16:32:18 +0200

    détection du premier mouvement et affichage lorsque le mouvement est effectué

---
+++ Assets/Script/Mouvement1.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouvement1 : MonoBehaviour {
+    public GameObject rightHand;
+    public float sensitivity;
+    public float movementSensitivity;
+
+    private Vector3 lastPos;
+    private Vector3 currPos;
+    private int goingX = 0;
+    private float startTime;
+    private Vector2 startPos;
+
+    // Use this for initialization
+    void Start () {
+        lastPos = rightHand.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currPos = rightHand.transform.position;
+
+        float diff = Mathf.Abs(currPos.x - lastPos.x);
+
+        //Debug.Log(currPos + " - " + lastPos);
+
+        if (currPos.x > lastPos.x && diff >= sensitivity)
+        {
+            //Debug.Log("to right");
+            if (goingX == -1)
+            {
+                //Debug.Log("left to right");
+                startTime = Time.time;
+                startPos = currPos;
+            }
+            goingX = 1;
+        } else if (currPos.x < lastPos.x && diff >= sensitivity)
+        {
+            //Debug.Log("to left");
+            if (goingX == 1)
+            {
+                //Debug.Log("right to left");
+                //Debug.Log(Vector2.Distance(currPos, startPos));
+                if(Vector2.Distance(currPos, startPos) < movementSensitivity)
+                {
+                    goingX = 0;
+                    startTime = 0f;
+                    startPos = new Vector2();
+                    //Debug.Log("faux mouvement");
+                }
+            }
+            if (Vector2.Distance(currPos, startPos) < movementSensitivity && startTime != 0)
+            {
+                goingX = 0;
+                startTime = 0f;
+                startPos = new Vector2();
+                //Debug.Log("finished");
+                GetComponent<TextDisplayer>().changeText("Mouvement 1");
+            }
+            goingX = -1;
+        }
+
+        lastPos = currPos;
+	}
+}
---
+++ Assets/Script/TextDisplayer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextDisplayer : MonoBehaviour {
+    public Text test;
+    public float delay;
+
+    private float startTime;
+
+	// Use this for initialization
+	void Start () {
+        delay = 1f;
+        startTime = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void changeText(string text)
+    {
+        if (text.Equals(test.text))
+        {
+            if (startTime == 0f)
+            {
+                Debug.Log("1");
+                startTime = Time.time;
+                test.color = Color.red;
+            }
+            if (startTime+delay >= Time.time)
+            {
+                Debug.Log("2");
+                test.color = Color.black;
+                startTime = 0f;
+            }
+
+        } else 
+            test.text = text;
+    }
+}
